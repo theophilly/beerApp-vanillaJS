85028995b4ff4fba16f5dc83216ce0483c0d1b82
theophilly <theophilly20@gmail.com>
2020-12-23 00:30:46 +0300

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let beerData;
 
 const fetchData = async () => {
+  document.getElementById("root").innerHTML = "";
   fetch("https://api.punkapi.com/v2/beers?per_page=6")
     .then(function (response) {
       return response.json();
@@ -39,16 +40,34 @@ const fetchData = async () => {
     });
 };
 
+//This would load the data on start
 fetchData();
 
-fetchdes = (data) => data.substring(0, 130) + "...";
+//This would load the beers when the logo is clicked
+document.querySelector(".logo").addEventListener("click", (event) => {
+  event.preventDefault();
+  fetchData();
+});
 
+//This would trim the desccription to 130 characters
+fetchdes = (data) => data.substring(0, 110) + "...";
+//This would trim the desccription to 130 characters
+fetchname = (data) => data.substring(0, 35);
+
+//This would query the search
 document.querySelector("button").addEventListener("click", (event) => {
   event.preventDefault();
 
   var searchTerm = document.querySelector("input").value;
 
   searchTerm = searchTerm.toString();
+  if (
+    searchTerm.toString() === null ||
+    searchTerm.toString().match(/^ *$/) !== null
+  ) {
+    alert("Search cannot be empty");
+    return;
+  }
 
   document.getElementById("root").innerHTML = "";
 
@@ -57,6 +76,26 @@ document.querySelector("button").addEventListener("click", (event) => {
       return response.json();
     })
     .then(function (arr) {
+      var check = Object.keys(arr).length;
+
+      if (check == 0) {
+        document.getElementById("root").innerHTML = "";
+        var root = document.getElementById("root");
+        root.innerHTML = ` <div class="noBeer">
+          <h3>Sorry! We dont have this type of beer</h3>
+          <button class="goBackBtn">
+          Go back Home  </button>  </div> `;
+
+        document
+          .querySelector(".goBackBtn")
+          .addEventListener("click", (event) => {
+            event.preventDefault();
+            fetchData();
+          });
+
+        return;
+      }
+
       arr.map((json) => {
         var root = document.getElementById("root");
         var beer = document.createElement("DIV");
@@ -64,7 +103,7 @@ document.querySelector("button").addEventListener("click", (event) => {
         beer.innerHTML = `    <i class="far fa-star icon"></i>
         <img src=${json.image_url} alt="" />
         <div class="description">
-          <h3 class="title">${json.name.toString()}</h3>
+          <h3 class="title">${fetchname(json.name.toString())}</h3>
           <p>
             ${fetchdes(json.description.toString())}
           </p>
@@ -72,6 +111,7 @@ document.querySelector("button").addEventListener("click", (event) => {
         console.log(beer);
         root.appendChild(beer);
       });
+
       var ele = document.querySelectorAll(".icon");
       ele.forEach((item) => {
         item.addEventListener("click", (event) => {
